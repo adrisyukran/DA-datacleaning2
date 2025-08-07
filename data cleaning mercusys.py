0e35437a99dae3a444ac1f06f1ee6847e1ceccee
Adri Syukran Saadon <adri.syukran17@gmail.com>
2025-08-07 23:20:41 +0800

    Update data cleaning mercusys.py

--- data cleaning mercusys.py
+++ data cleaning mercusys.py
@@ -11,14 +11,17 @@ df['price'] = df['price'].replace('[RM,\s]', '', regex=True).astype(float)
 # Clean the 'prod-weight' column by removing 'Weight:' and any non-numeric characters, then convert to float
 df['prod-weight'] = df['prod-weight'].replace('Weight:\s*', '', regex=True)
 df['prod-weight'] = df['prod-weight'].replace('[kg,\s]', '', regex=True).astype(float)
+df['prod-weight'] = df['prod-weight'].fillna(0) 
 
 ## in-stock Cleaning
 # Clean 'in-stock' column by replacing 'In stock' with 1 and 'Out of stock' with 0
 df['in-stock'] = df['in-stock'].replace({'In Stock': 1, 'Out of Stock': 0})
-# Convert 'in-stock' NaN values to 1
-df['in-stock'] = df['in-stock'].fillna(1).astype(float)
+# Convert 'in-stock' NaN values to 1 and ensure it's an integer type
+df['in-stock'] = df['in-stock'].fillna(1).astype(int)
 # Convert 'in-stock' to numeric, coercing errors to NaN
 df['in-stock'] = pd.to_numeric(df['in-stock'], errors='coerce')
+
+
 
 
 
